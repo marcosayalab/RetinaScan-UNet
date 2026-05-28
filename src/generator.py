98c18e979ecff27eb8be388f1d3a54b71de66742
GBK4935 <marayabla@alum.us.es>
2026-05-28 17:24:31 +0200

    feat: Add new image and mask files for training and testing; update data generator for image processing
    - Added 40 new training images and masks in TIFF and GIF formats.
    - Added 20 new test images and masks in TIFF and GIF formats.
    - Updated the data generator to read images using OpenCV and PIL, normalize pixel values, and implement random patch extraction with optional data augmentation.
    - Enhanced the exploration notebook with image loading and normalization examples.

--- src/generator.py
+++ src/generator.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import random
 from tensorflow import keras
+from PIL import Image
 
 class DataGenerator(keras.utils.PyDataset):
     def __init__(self, rutas_imagenes, rutas_mascaras, batch_size=4, patch_size=(128, 128), workers=1, use_multiprocessing=False):
@@ -38,33 +39,43 @@ class DataGenerator(keras.utils.PyDataset):
         # 3. Procesar cada imagen del lote actual
         for i, (ruta_img, ruta_mask) in enumerate(zip(rutas_batch_x, rutas_batch_y)):
             
-            # TODO 1: Leer las imágenes del disco duro
+            # 1: Leer las imágenes del disco duro
             # Pista: Usa cv2.imread(). ¡Cuidado! OpenCV lee en BGR, quizás quieras pasarlo a RGB.
             # Para la máscara, léela en escala de grises (cv2.IMREAD_GRAYSCALE).
-            img = ... 
-            mask = ...
+            ojo_bgr = cv2.imread(ruta_img) 
+            ojo_rgb = cv2.cvtColor(ojo_bgr, cv2.COLOR_BGR2RGB)
+            mask_pil = Image.open(ruta_mask)
+            mask_array = np.array(mask_pil)
 
-            # TODO 2: Normalización
+            # 2: Normalización
             # Las imágenes vienen con píxeles de 0 a 255. Divídelas para que estén entre 0.0 y 1.0.
-            img_normalizada = ...
-            mask_normalizada = ...
+            img_normalizada = ojo_rgb / 255.0
+            mask_normalizada = mask_array / 255.0
 
-            # TODO 3: El Parcheado (Troceado coordinado)
+            # 3: El Parcheado (Troceado coordinado)
             # Tienes que generar unas coordenadas X e Y aleatorias.
             # Asegúrate de que las coordenadas no se salgan de los bordes de la imagen original
             # al sumarle los 128 píxeles del patch_size.
             # Luego, recorta EXACTAMENTE las mismas coordenadas tanto en 'img' como en 'mask'.
-            x_random = ...
-            y_random = ...
-            
+
+            alto_original, ancho_original = img_normalizada.shape[:2]
+            limite_y = alto_original - self.patch_size[0]
+            limite_x = ancho_original - self.patch_size[1]
+
+            x_random = random.randint(0, limite_x)
+            y_random = random.randint(0, limite_y)
+
             parche_img = img_normalizada[y_random : y_random + self.patch_size[0], x_random : x_random + self.patch_size[1]]
             parche_mask = mask_normalizada[y_random : y_random + self.patch_size[0], x_random : x_random + self.patch_size[1]]
 
-            # TODO 4: Data Augmentation (Opcional pero muy recomendado)
+            # 4: Data Augmentation (Opcional pero muy recomendado)
             # Genera un número aleatorio. Si es mayor que 0.5, dale la vuelta horizontalmente (cv2.flip)
             # tanto al parche de la imagen como al de la máscara.
-            
-            # 4. Guardar los parches finales en las matrices del batch
+            if random.random() > 0.5: 
+                parche_img = cv2.flip(parche_img, 1)  # 1 para flip horizontal
+                parche_mask = cv2.flip(parche_mask, 1)
+
+            # 5. Guardar los parches finales en las matrices del batch
             X[i,] = parche_img
             
             # OpenCV devuelve matrices de (128, 128) al leer en escala de grises. 
