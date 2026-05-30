8900fe3bae32cd71b80f04771e09fd377ab8af3e
GBK4935 <marayabla@alum.us.es>
2026-05-30 11:43:45 +0200

    fix: Update import statement for Keras to improve compatibility

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import os
 import glob
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+import keras
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.optimizers import Adam
 from sklearn.model_selection import KFold
